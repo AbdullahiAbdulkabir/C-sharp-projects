ef0558542009ca01a9c6d719a33b161e5863d4f1
Abdullahi Abdulkabir <33360580+AbdullahiAbdulkabir@users.noreply.github.com>
2022-08-12 15:37:15 +0100

    Hello world

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //{  int[] numbers = new int[] {1,2,3,4,7,9};
+        //Console.WriteLine(numbers[2]);
+        // Convert.ToInt32( Console.ReadLine());
+            int x = 0;
+    do
+         Console.WriteLine("x is {0}", x++);
+          while (x<3);
+    Console.ReadLine();
+
+        }
+    }
+}
