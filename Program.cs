ddaa257e9796a2c5cb33e854ad3ec3581f8c8c42
Abdullahi Abdulkabir <33360580+AbdullahiAbdulkabir@users.noreply.github.com>
2022-08-12 15:24:25 +0100

    Return the positions of each value

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classpractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Put your input");
+           // string num =  Console.ReadLine();
+            Console.WriteLine("Input 2 digits number");
+
+            decimal num1 = int.Parse( Console.ReadLine());
+            int tens = (int) Math.Floor(num1 / 10);
+            int unit =(int) num1 %10;
+            Console.WriteLine("The tenth unit is {0} and the unit is {1}",tens,unit);
+            //Console.WriteLine("The tenth unit is {0} and the unit is {1}",num[0],num[1]);
+            int[, ,] firstArray= new int[1,3,4];
+            
+            Console.WriteLine(firstArray);
+            Console.ReadLine();
+
+        }
+    }
+}
